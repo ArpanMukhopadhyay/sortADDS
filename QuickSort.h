23355b5eff85826249a42d53bb55123102761813
Arpan Mukhopadhyay <80723259+ArpanMukhopadhyay@users.noreply.github.com>
2022-05-16 22:50:53 +0930

    Add files via upload

--- QuickSort.h
+++ QuickSort.h
@@ -1,18 +1,16 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
-
 #include "Sort.h"
 #include <vector>
 
 class QuickSort: public Sort{
-    QuickSort();
-    ~QuickSort();
-
-    void exec(std::vector<int> &toSort, int, int);
+    public:
+        QuickSort();
+        void exec(std::vector<int> &toSort, int, int);
+        ~QuickSort();
     
     private:
         int Split(std::vector<int> &toSort, int, int, int);
 };
-
 
 #endif
